5636e37744d0dc3f1f8cfc1c30d02600d0d8437c
clicktx <clicktx@gmail.com>
2016-03-11 16:59:42 +0900

    fixed tidy

--- lib/Markets/Web/C/Root.pm
+++ lib/Markets/Web/C/Root.pm
@@ -4,25 +4,27 @@ use warnings;
 use utf8;
 
 sub index {
-    my ($class, $c) = @_;
+    my ( $class, $c ) = @_;
 
     my $model = $c->model('Data::Foo');
     my $logic = $c->model('Logic::Hoge');
 
     my $counter = $c->session->get('counter') || 0;
     $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
-        keys => {
-            model => $model->hello,
-            logic => $logic->hoge,
-        },
-    });
+    $c->session->set( 'counter' => $counter );
+    return $c->render(
+        'index.tx',
+        {   counter => $counter,
+            keys    => {
+                model => $model->hello,
+                logic => $logic->hoge,
+            },
+        }
+    );
 }
 
 sub reset_counter {
-    my ($class, $c) = @_;
+    my ( $class, $c ) = @_;
 
     $c->session->remove('counter');
     return $c->redirect('/');
