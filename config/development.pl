4e63a9ae39135c1683ade533db9796a6f48a498b
clicktx <clicktx@gmail.com>
2016-03-18 22:57:06 +0900

    added base config file

--- config/development.pl
+++ config/development.pl
@@ -2,7 +2,9 @@ use File::Spec;
 use File::Basename qw(dirname);
 my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
 my $dbpath = File::Spec->catfile($basedir, 'db', 'development.db');
+my $base_config = do File::Spec->catfile(dirname(__FILE__), 'base.pl');
 +{
+    %$base_config,
     'DBI' => [
         "dbi:SQLite:dbname=$dbpath", '', '',
         +{
